835d1f2e0ccb8ff9cf74a974b0d93d97c01d78e3
Anuj Chatterjee <anujchatterjee@gmail.com>
2021-01-01 22:27:07 -0800

    get all the candidates

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//import the sqlite3 package 
+const sqlite3 = require('sqlite3').verbose();
 // Open the server.js file and import express at the top of the file
 const express = require('express');
 // the PORT designation and the app expression
@@ -7,6 +9,61 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+      return console.error(err.message);
+    }
+  
+    console.log('Connected to the election database.');
+  });
+// return all the data in the candidates table
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql,params, (err, rows) => {
+        if(err){
+            res.status(500).json({error:err.message});
+        return ;
+        }
+        res.json({
+            message:'success',
+            data:rows
+        });
+    });
+});
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+// Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+//   });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -20,6 +77,9 @@ app.use((req, res) => {
   });
 
 // the function that will start the Express.js server on port 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   });
