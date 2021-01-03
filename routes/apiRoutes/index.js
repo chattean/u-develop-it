7577029d8f4523fb673c402b7e43ada010a58f4b
Anuj Chatterjee <anujchatterjee@gmail.com>
2021-01-02 16:40:28 -0800

    Added all the api routes and modularized the code

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+// Open the server.js file and import express at the top of the file
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
+module.exports = router;
